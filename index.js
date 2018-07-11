d5672a1497b3b72d9d6fcf46e3948342a5bab3a2
Andrew Thompson <andrew.thompson@qmul.ac.uk>
2018-07-11 09:30:08 +0100

    :wrench: Create Heroku procfile.

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const app = require('express')()
 const http = require('http').Server(app)
+const socket = require('./socket')
 
 const host = '0.0.0.0'
 const port = process.env.PORT || 5000
 
 app.set('port', port)
+socket.begin(http)
 http.listen(port, host)
 
 console.log('Server listening on ' + host + ':' + port)
