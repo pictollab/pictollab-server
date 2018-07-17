e3be2acd7e985d9efe0fda72b18081d5d2a1c7e8
Andrew Thompson <andrew.thompson@qmul.ac.uk>
2018-07-17 14:34:29 +0100

    :rewind: Rolling back changes.

--- socket/event.js
+++ socket/event.js
@@ -1,20 +1,3 @@
-const http = require('http')
-
 module.exports = function (io, socket, data) {
-  const bodyString = JSON.stringify(data)
-
-  const headers = {
-      'Content-Type': 'application/json',
-      'Content-Length': bodyString.length
-  }
-
-  const options = {
-      host: 'https://pictollab.localtunnel.me',
-      path: '/log/user',
-      port: 8080,
-      method: 'PUT',
-      headers: headers
-  }
-
-  http.request(options, () => {}).write(bodyString)
+  console.log(data)
 }
