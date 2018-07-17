06649740b809d92e9e860d568d845da81576e8f7
Andrew Thompson <andrew.thompson@qmul.ac.uk>
2018-07-17 14:32:13 +0100

    :construction: Use default http library to HTTP POST log info.

--- socket/event.js
+++ socket/event.js
@@ -1,3 +1,20 @@
-module.exports = function (io, socket, data) {
+const http = require('http')
 
+module.exports = function (io, socket, data) {
+  const bodyString = JSON.stringify(data)
+
+  const headers = {
+      'Content-Type': 'application/json',
+      'Content-Length': bodyString.length
+  }
+
+  const options = {
+      host: 'https://pictollab.localtunnel.me',
+      path: '/log/user',
+      port: 8080,
+      method: 'PUT',
+      headers: headers
+  }
+
+  http.request(options, () => {}).write(bodyString)
 }
