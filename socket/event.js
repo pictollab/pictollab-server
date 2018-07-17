0c98b859073f16138dbbdbebce9bdafdb9d49844
Andrew Thompson <andrew.thompson@qmul.ac.uk>
2018-07-17 14:28:45 +0100

    :heavy_minus_sign: Remove request dependency.

--- socket/event.js
+++ socket/event.js
@@ -1,9 +1,3 @@
-const request = require('request')
-
 module.exports = function (io, socket, data) {
-  request({
-    url: 'pictollab.localtunnel.me/log/user',
-    method: 'POST',
-    json: data
-  })
+
 }
