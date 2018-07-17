5e21a8694e75ca9cc6ed2d8155d065cc8513e085
Andrew Thompson <andrew.thompson@qmul.ac.uk>
2018-07-17 15:07:49 +0100

    :construction: Connect to temporary localtunnel endpoint.

--- socket/event.js
+++ socket/event.js
@@ -1,3 +1,13 @@
+const axios = require('axios')
+
 module.exports = function (io, socket, data) {
   console.log(data)
+
+  axios.post('https://https://cowardly-quail-63.localtunnel.me/log/user', data)
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
 }
