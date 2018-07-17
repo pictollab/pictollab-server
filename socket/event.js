151e2f442fd91b0dbbb62ad8f271a36e92aa6112
Andrew Thompson <andrew.thompson@qmul.ac.uk>
2018-07-17 15:09:55 +0100

    :bug: Fixed duplicate https:// in url.

--- socket/event.js
+++ socket/event.js
@@ -3,7 +3,7 @@ const axios = require('axios')
 module.exports = function (io, socket, data) {
   console.log(data)
 
-  axios.post('https://https://cowardly-quail-63.localtunnel.me/log/user', data)
+  axios.post('https://cowardly-quail-63.localtunnel.me/log/user', data)
   .then(function (response) {
     console.log(response);
   })
