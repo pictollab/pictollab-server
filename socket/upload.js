8955029654f665f50b0cec224b4c78e04dbf3a00
Andrew Thompson <andrew.thompson@qmul.ac.uk>
2018-07-30 11:06:08 +0100

    :construction: Add console logs to check incoming data for upload event.

--- socket/upload.js
+++ socket/upload.js
@@ -2,18 +2,20 @@ const axios = require('axios')
 const time = require('../utils/time')
 
 module.exports = function (io, socket, data) {
-  axios.post(`https://${ process.env.LOG_ENDPOINT }.localtunnel.me/log/server/event`, {
-    id: socket.id,
-    event: {
-      timestamp: {
-        client: data.timestamp.client,
-        server: Date.now()
-      }
-    }
-  }).then(function (res) {
-    // console.log(res);
-  })
-  .catch(function (err) {
-    // console.log(err);
-  })
+  const { type, timestamp } = data
+  console.log(type, timestamp)
+  // axios.post(`https://${ process.env.LOG_ENDPOINT }.localtunnel.me/log/server/event`, {
+  //   id: socket.id,
+  //   event: {
+  //     timestamp: {
+  //       client: data.timestamp.client,
+  //       server: Date.now()
+  //     }
+  //   }
+  // }).then(function (res) {
+  //   // console.log(res);
+  // })
+  // .catch(function (err) {
+  //   // console.log(err);
+  // })
 }
