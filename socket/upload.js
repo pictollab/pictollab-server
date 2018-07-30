8493fee790aa6f6024669841d6a831548defc8c9
Andrew Thompson <andrew.thompson@qmul.ac.uk>
2018-07-30 10:41:29 +0100

    :sparkles: Send upload event to logging server to calculate latency.

--- socket/upload.js
+++ socket/upload.js
@@ -1,3 +1,16 @@
+const axios = require('axios')
+const time = require('../utils/time')
+
 module.exports = function (io, socket, data) {
-  // console.log('Upload')
+  data.timestamp.server = time.now()
+
+  axios.post(`https://${ process.env.LOG_ENDPOINT }.localtunnel.me/log/server/event`, {
+    id: socket.id,
+    event: data
+  }).then(function (res) {
+    // console.log(res);
+  })
+  .catch(function (err) {
+    // console.log(err);
+  })
 }
