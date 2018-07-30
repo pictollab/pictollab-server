1117573d71cab11e205de54a1af49f3fe0d6db70
Andrew Thompson <andrew.thompson@qmul.ac.uk>
2018-07-30 10:49:38 +0100

    :construction: Add console.log

--- socket/upload.js
+++ socket/upload.js
@@ -2,7 +2,8 @@ const axios = require('axios')
 const time = require('../utils/time')
 
 module.exports = function (io, socket, data) {
-  data.timestamp.server = time.now()
+  console.log(data)
+  // data.timestamp.server = time.now()
 
   axios.post(`https://${ process.env.LOG_ENDPOINT }.localtunnel.me/log/server/event`, {
     id: socket.id,
