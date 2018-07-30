f3d4cd2aee33fc263d7f1281206fc6cd0d66b46b
Andrew Thompson <andrew.thompson@qmul.ac.uk>
2018-07-30 11:09:24 +0100

    :construction: Send upload timestamps to logging server.

--- socket/upload.js
+++ socket/upload.js
@@ -3,19 +3,17 @@ const time = require('../utils/time')
 
 module.exports = function (io, socket, data) {
   const { type, timestamp } = data
-  console.log(type, timestamp)
-  // axios.post(`https://${ process.env.LOG_ENDPOINT }.localtunnel.me/log/server/event`, {
-  //   id: socket.id,
-  //   event: {
-  //     timestamp: {
-  //       client: data.timestamp.client,
-  //       server: Date.now()
-  //     }
-  //   }
-  // }).then(function (res) {
-  //   // console.log(res);
-  // })
-  // .catch(function (err) {
-  //   // console.log(err);
-  // })
+  timestamp.server = Date.now()
+  axios.post(`https://${ process.env.LOG_ENDPOINT }.localtunnel.me/log/server/event`, {
+    id: socket.id,
+    event: { 
+      type,
+      timestamp
+     }
+  }).then(function (res) {
+    // console.log(res);
+  })
+  .catch(function (err) {
+    // console.log(err);
+  })
 }
